6dc1854cd4254436ad07dd1969203aa02226ce82
heesun91 <68679866+heesun91@users.noreply.github.com>
2020-12-05 15:27:17 +0900

    Add files via upload
    Hash2

---
+++ Programmers/PhoneList.java
@@ -0,0 +1,42 @@
+package phoneList;
+
+import java.util.HashMap;
+
+public class PhoneList {
+	
+	 public static void main(String[] args) {
+			
+			// TODO Auto-generated method stub
+			
+			String[] arr1 = {"119","976716","11955432321","213213"};
+			String[] arr2 = {"123","456","789"};
+			
+			
+			System.out.println(solution(arr2));
+
+		}
+	
+	
+	    public static boolean solution(String[] phone_book) {
+	        boolean answer = true;
+	        
+	        HashMap <String, String> hashMap = new HashMap<>();
+	        
+	        for(int i=0; i<phone_book.length; i++) {
+	        	hashMap.put(phone_book[i], "prefix");
+	        }
+	        
+	        for(String phone : phone_book) {
+	        	for(int idx=0; idx<phone.length();idx++) {
+	        		if(hashMap.containsKey(phone.substring(0,idx))) {
+	        			answer= false;
+	        		}
+	        	}
+	        }
+	        return answer;
+	    }
+	
+	
+	
+
+}
